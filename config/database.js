bfc4e804f9545adfa028b4e52b9178d050a19322
GAMATAM VAJINTHA <gamatam.vajintha@zysk.tech>
2023-08-30 11:17:57 +0530

    forked files added

--- config/database.js
+++ config/database.js
@@ -1,17 +1,33 @@
-module.exports = ({ env }) => ({
-  connection:{
-       client: 'postgres',
-       connection:{
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'aspinodb'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'Vajintha@1511'),
-        schema: env('DATABASE_SCHEMA', 'public'),
-         ssl: env.bool('DATABASE_SSL',true),
-      },
-    },
-});
+// module.exports = ({ env }) => ({
+//   connection:{
+//        client: 'postgres',
+//        connection:{
+//         host: env('DATABASE_HOST', 'localhost'),
+//         port: env.int('DATABASE_PORT', 5432),
+//         database: env('DATABASE_NAME', 'aspinodb'),
+//         user: env('DATABASE_USERNAME', 'postgres'),
+//         password: env('DATABASE_PASSWORD', 'Vajintha@1511'),
+//         schema: env('DATABASE_SCHEMA', 'public'),
+//          ssl: env.bool('DATABASE_SSL',true),
+//       },
+//     },
+// });
 
 
 // npm install pg --save //command used to run the postgres
+
+module.exports = ({ env }) => ({
+
+  connection: {
+    client: env('DATABASE_CLIENT'),
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      schema: env('DATABASE_SCHEMA'),
+      ssl: env.bool('DATABASE_SSL'),
+    },
+  }
+})
